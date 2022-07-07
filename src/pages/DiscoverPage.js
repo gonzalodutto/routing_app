a91d216771067fa42b87c14db8fecaa389f0e4ae
gonzalodutto <gonzalodutto@gmail.com>
2022-07-07 11:49:46 +0200

    Creating Routes

---
+++ src/pages/DiscoverPage.js
@@ -0,0 +1,5 @@
+const DiscoverPage = () => {
+  return <p> This is the discover page </p>;
+};
+
+export default DiscoverPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <p> This is the HOME page </p>;
+};
+
+export default HomePage;
